86f6962dd13da4e1654180224b5f8adb19b1f448
Harsh Agarwal <harshagarwal8002@gmail.com>
2024-12-29 13:39:05 +0530

    add another source

--- 01react-vite/src/App.jsx
+++ 01react-vite/src/App.jsx
@@ -1,11 +1,11 @@
 
-
+import Fz from './another.jsx'
 function App() {
  
 
   return (
     <>
-    <h1>anjali pagal is pagal from agra pagal khana who always watch r</h1>
+    <Fz/>
     </>
   )
 }
---
+++ 01react-vite/src/another.jsx
@@ -0,0 +1,12 @@
+const Fz = ()=>{
+    return(
+    
+    
+    <h1>anjali pagal is pagal from agra pagal khana who always watch r</h1>
+    );
+    
+        
+    
+}
+
+export default Fz
